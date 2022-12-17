8316ee1af291efa5bfed66c8aa1738dd9d3f0e2c
Jaime-Dantas <jaimesilvajk@gmail.com>
2022-12-16 22:40:26 -0300

    estilização

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+//criação da classe
+class Produto {
+
+  // Metodo ou função
+  constructor(){
+
+  }
+
+  cadastrar(){
+    alert('Cadastrado com sucesso!');
+  }
+
+  excluir(){
+    alert('Excluido com sucesso!');
+  }
+
+}
+
+var produto = new Produto();
